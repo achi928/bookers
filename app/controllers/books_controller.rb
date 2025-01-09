411592f9a0ee56780eb5a752d7b37df85dde949d
EC2 Default User <ec2-user@ip-172-31-13-31.ap-northeast-1.compute.internal>
2025-01-09 09:08:17 +0000

    レイアウト整える前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,23 @@
 class BooksController < ApplicationController
+  protect_from_forgery
   def new
-    @book = Book.new
+    
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to 'book_path'(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -19,14 +26,28 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-  def destory
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end 
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
